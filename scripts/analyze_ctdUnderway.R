88444521625b4c3a78017673338e65c338cabc17
Helen Killeen <hjkilleen@ucdavis.edu>
2020-10-14 15:57:48 -0700

    plot map

--- scripts/analyze_ctdUnderway.R
+++ scripts/analyze_ctdUnderway.R
@@ -2,8 +2,9 @@
 
 #LIBRARIES
 library(lubridate)
-source("scripts/")
-
+library(ggplot2)
+source("scripts/functions/environmental.utils.R")
+  
 #LOAD DATA
 filenames <- list.files("data/environment/CTD/cleaned/Underway/", full.names = TRUE)
 ldf <- lapply(filenames, read.table)
@@ -18,13 +19,18 @@ for(i in seq(1:9)){if(ncol(ldf[[i]]) == 10){
   else {
     names(ldf[[i]]) <- names8
   }}
+#set classes
+for(i in seq(1:9)){
+  ldf[[1]]$latitude <- as.numeric(as.character(ldf[[i]]$latitude))
+  ldf[[i]]$longitude <- as.numeric(as.character(ldf[[i]]$longitude))
+  ldf[[i]]$density <- as.numeric(as.character(ldf[[i]]$density))
+}
 #add PDT time
 for(i in seq(1:9)){
   ldf[[i]]$datetime <- paste(ldf[[i]]$date, ldf[[i]]$time_UTC, sep = " ")
   ldf[[i]]$datetime <- parse_date_time(ldf[[i]]$datetime, orders = "mdy HMS")
   ldf[[i]]$datetime_PDT <- with_tz(ldf[[i]]$datetime, "America/Los_Angeles")
 }
-View(ldf[[2]])
 #start and end times that pair with list items
 times <- data.frame(
   start.times_PDT = as.POSIXct(c("2019-08-15 10:45:00 PDT", "2019-09-20 9:21:00 PDT", "2020-06-23 11:39:00 PDT", "2020-06-30 10:26:00 PDT", "2020-07-28 10:30:00 PDT", "2020-09-03 11:03:00 PDT", "2020-09-30 10:30:00 PDT", "2020-10-06 9:47:00 PDT", "2020-10-06 10:47:00 PDT")),
@@ -37,3 +43,11 @@ for(i in seq(1:9)){
 }
 
 #PLOTS
+#this doesn't work
+#lapply(ldf, map.underway)
+
+ggplot(ldf[[1]], aes(x = longitude, y = latitude, color = density))+
+  geom_point(size = 3)+
+  scale_color_gradient2(low = scales::muted("blue"), mid = "white", high = scales::muted("red"), midpoint = mean(ldf[[1]]$density)) +
+  labs(x = "Longitude", y = "Latitude", title = paste(ldf[[1]]$date[1], "Surface Density", sep = " ")) 
+
