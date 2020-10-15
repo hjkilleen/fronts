4319483ee080835f56e762f64344db95660a4895
Helen Killeen <hjkilleen@ucdavis.edu>
2020-10-15 07:09:08 -0700

    Save figures for cruise 3 and 6.

--- scripts/analyze_ctdUnderway.R
+++ scripts/analyze_ctdUnderway.R
@@ -3,6 +3,7 @@
 #LIBRARIES
 library(lubridate)
 library(ggplot2)
+library(dplyr)
 source("scripts/functions/environmental.utils.R")
   
 #LOAD DATA
@@ -21,9 +22,9 @@ for(i in seq(1:9)){if(ncol(ldf[[i]]) == 10){
   }}
 #set classes
 for(i in seq(1:9)){
-  ldf[[1]]$latitude <- as.numeric(as.character(ldf[[i]]$latitude))
-  ldf[[i]]$longitude <- as.numeric(as.character(ldf[[i]]$longitude))
-  ldf[[i]]$density <- as.numeric(as.character(ldf[[i]]$density))
+  ldf[[4]]$latitude <- as.numeric(as.character(ldf[[4]]$latitude))
+  ldf[[4]]$longitude <- as.numeric(as.character(ldf[[4]]$longitude))
+  ldf[[4]]$density <- as.numeric(as.character(ldf[[4]]$density))
 }
 #add PDT time
 for(i in seq(1:9)){
@@ -45,9 +46,31 @@ for(i in seq(1:9)){
 #PLOTS
 #this doesn't work
 #lapply(ldf, map.underway)
+load(file = "data/environment/mapping/flah_map_data.R")
 
-ggplot(ldf[[1]], aes(x = longitude, y = latitude, color = density))+
-  geom_point(size = 3)+
-  scale_color_gradient2(low = scales::muted("blue"), mid = "white", high = scales::muted("red"), midpoint = mean(ldf[[1]]$density)) +
-  labs(x = "Longitude", y = "Latitude", title = paste(ldf[[1]]$date[1], "Surface Density", sep = " ")) 
+ggplot(filter(ldf[[1]], density>1024.8, density <1025.6))+
+  geom_point(size = 3, shape = 21, color = "grey", aes(x = longitude, y = latitude, fill = density))+
+  scale_fill_gradient2(low = "blue", mid = "white", high = "red", midpoint = mean(ldf[[1]]$density)) +
+  geom_point(data = coastline.df %>% dplyr::filter(long < max(ldf[[1]]$longitude) + 0.0075, long > min(ldf[[1]]$longitude), lat > min(ldf[[1]]$latitude) - 0.25, lat < max(ldf[[1]]$latitude)), aes(x = long, y = lat), size = 0.1) +
+  labs(x = "Longitude", y = "Latitude", title = paste(ldf[[1]]$date[1], "Surface Density", sep = " ")) +
+  geom_segment(data = data.frame( y = 38.32622, x = -123.07793, yend = 38.32713, xend = -123.0846),
+               aes(x = x, y = y, xend = xend, yend = yend), color = "black", size = 1, inherit.aes = FALSE) + 
+  geom_segment(data = data.frame( y = 38.32466, x = -123.0817, yend = 38.3261, xend = -123.08662),
+               aes(x = x, y = y, xend = xend, yend = yend), color = "black", size = 1, inherit.aes = FALSE) +
+  geom_segment(data = data.frame( y = 38.32374, x = -123.08311, yend = 38.32505, xend = -123.08913),
+               aes(x = x, y = y, xend = xend, yend = yend), color = "black", size = 1, inherit.aes = FALSE) +
+  ggsave(file = "figures/ctdUnderway/8.15.2019.jpg")
 
+ggplot(filter(ldf[[4]], density>1024.7))+
+  geom_point(size = 3, shape = 21, color = "grey", aes(x = longitude, y = latitude, fill = density))+
+  scale_fill_gradient2(low = "blue", mid = "white", high = "red", midpoint = mean(ldf[[4]]$density)) +
+  geom_point(data = coastline.df %>% dplyr::filter(long < max(ldf[[4]]$longitude) + 0.0075, long > min(ldf[[4]]$longitude), lat > min(ldf[[4]]$latitude) - 0.25, lat < max(ldf[[4]]$latitude)), aes(x = long, y = lat), size = 0.1) +
+  labs(x = "Longitude", y = "Latitude", title = paste(ldf[[4]]$date[1], "Surface Density", sep = " ")) +
+  geom_segment(data = data.frame( y = 38.32622, x = -123.07793, yend = 38.32713, xend = -123.0846),
+               aes(x = x, y = y, xend = xend, yend = yend), color = "black", size = 1, inherit.aes = FALSE) + 
+  geom_segment(data = data.frame( y = 38.32466, x = -123.0817, yend = 38.3261, xend = -123.08662),
+               aes(x = x, y = y, xend = xend, yend = yend), color = "black", size = 1, inherit.aes = FALSE) +
+  geom_segment(data = data.frame( y = 38.32374, x = -123.08311, yend = 38.32505, xend = -123.08913),
+               aes(x = x, y = y, xend = xend, yend = yend), color = "black", size = 1, inherit.aes = FALSE) +
+  geom_point(data = data.frame(lon = c(-123.08422, -123.08327, -123.08344, -123.08489, -123.08164), lat = c(38.32222, 38.32371, 38.32461, 38.32648, 38.33100)), aes(x = lon, y = lat), color = "black", shape = 4, size = 5) +
+  ggsave(file = "figures/ctdUnderway/6.30.2020.jpg")
