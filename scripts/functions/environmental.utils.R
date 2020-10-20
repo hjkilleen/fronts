c14ecc8fa229fd82cbb0db9c95f839813b6dc6f0
Helen Killeen <hjkilleen@ucdavis.edu>
2020-10-19 17:13:29 -0700

    Function for underway map (doesn't work as is).

--- scripts/functions/environmental.utils.R
+++ scripts/functions/environmental.utils.R
@@ -1,4 +1,8 @@
+library(ggplot2)
 #Plot Underway Map
 map.underway <- function(x){
-  
+  ggplot(ldf[[x]], aes(x = longitude, y = latitude, color = density))+
+    geom_point(size = 3)+
+    scale_color_gradient2(low = scales::muted("blue"), mid = "white", high = scales::muted("red"), midpoint = mean(ldf[[x]]$density)) +
+    labs(x = "Longitude", y = "Latitude", title = paste(ldf[[x]]$date[1], "Surface Density", sep = " ")) 
 }
