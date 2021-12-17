b03ba48c2b616261f2fe60448934253faae8c717
Helen Killeen <hjkilleen@ucdavis.edu>
2021-12-17 08:20:46 -0800

    load all cruises 1-6 (not 2) and bind

---
+++ scripts/data_load_plankton.R
@@ -0,0 +1,49 @@
+#Script to load plankton data
+
+# Tue Dec 14 09:17:03 2021 ------------------------------
+
+#LIBRARIES & SOURCES
+#====
+library(tidyverse)
+library(readxl)
+#====
+
+#LOAD DATA
+#====
+#load cruise 3
+sheet <- excel_sheets("data/biology/counts/07182019.xlsx")
+cruise1 <- lapply(setNames(sheet, sheet), 
+                  function(x) read_excel("data/biology/counts/07182019.xlsx", sheet=x))
+cruise1 <- bind_rows(cruise1, .id="Sheet")
+save(cruise1, file = "data/biology/counts/cruise1.rda")
+
+sheet <- excel_sheets("data/biology/counts/08152019.xlsx")
+cruise3 <- lapply(setNames(sheet, sheet), 
+                    function(x) read_excel("data/biology/counts/08152019.xlsx", sheet=x))
+cruise3 <- bind_rows(cruise3, .id="Sheet")
+save(cruise3, file = "data/biology/counts/cruise3.rda")
+
+sheet <- excel_sheets("data/biology/counts/09202019.xlsx")
+cruise4 <- lapply(setNames(sheet, sheet), 
+                  function(x) read_excel("data/biology/counts/09202019.xlsx", sheet=x))
+cruise4 <- bind_rows(cruise4, .id="Sheet")
+save(cruise4, file = "data/biology/counts/cruise4.rda")
+
+sheet <- excel_sheets("data/biology/counts/06232020.xlsx")
+cruise5 <- lapply(setNames(sheet, sheet), 
+                  function(x) read_excel("data/biology/counts/06232020.xlsx", sheet=x))
+cruise5 <- bind_rows(cruise5, .id="Sheet")
+save(cruise5, file = "data/biology/counts/cruise5.rda")
+
+sheet <- excel_sheets("data/biology/counts/06302020.xlsx")
+cruise6 <- lapply(setNames(sheet, sheet), 
+                  function(x) read_excel("data/biology/counts/06302020.xlsx", sheet=x))
+cruise6 <- bind_rows(cruise6, .id="Sheet")
+save(cruise6, file = "data/biology/counts/cruise6.rda")
+
+#Bind all cruises
+plankton <- bind_rows(cruise1, cruise3, cruise4, cruise5, cruise6)
+save(plankton, file = "data/biology/counts/plankton.rda")
+#====
+
+
