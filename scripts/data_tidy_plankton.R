2ed535c15a3e1d44b7f3e08b966ab2ee8884bdd6
Helen Killeen <hjkilleen@ucdavis.edu>
2022-04-14 22:54:44 -0700

    create species_stage moniker

--- scripts/data_tidy_plankton.R
+++ scripts/data_tidy_plankton.R
@@ -9,12 +9,20 @@ library(ggplot2)
 load("data/biology/counts/plankton.rda")
 #====
 
+#SET UP
+#====
+#eliminate unused 'sheet' column
+plankton <- plankton[,2:10]
+#create combined species_stage moniker
+plankton$spStage <- paste(plankton$species, plankton$stage, sep = "_")
+#====
+
 #DERIVED VARIABLES
 #====
-#eliminate unused columns
-plankton <- plankton[,2:10]
 #create a new variable for count * split (count/five min. tow)
 plankton$total <- plankton$count*plankton$split
+#create observational tally to estimate frequency of species_stage in dataset
+tally <- tally(group_by(plankton, spStage))
 #====
 
 
