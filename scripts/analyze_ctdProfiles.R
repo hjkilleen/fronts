b7a6be2d54651324c68b0bb437b437c1ac58d899
Helen Killeen <hjkilleen@ucdavis.edu>
2022-11-08 10:17:05 -0800

    code to get cruise plots for ctd profiles

--- scripts/analyze_ctdProfiles.R
+++ scripts/analyze_ctdProfiles.R
@@ -1,6 +1,9 @@
 # Thu Oct 15 07:11:15 2020 ------------------------------
 
 library(tidyverse)
+library(MBA)
+
+load("data/environment/CTD/cleaned/Profiles/allCTD.rda")
 
 #CTD plots
 names <- c( "date", "time_UTC", "pressure", "temp", "cond", "depth", "sal", "density")
@@ -25,4 +28,37 @@ ggplot(df) +
   scale_y_reverse() +
   facet_wrap(~pos)
 
-                     
+
+
+
+results <- "figures/ctdProfiles/"
+temp_list <- list()
+
+ctdDates <- as.POSIXct(c("2019-09-20", "2020-06-23", "2020-06-30", "2020-09-30"), tz = "America/Los_Angeles")
+for(i in 1:length(ctdDates)){#Interpolate and create plots for each cruise
+  d <- filter(allCTD, date == ctdDates[i])
+  d <- na.omit(d)#remove NAs
+  mba <- mba.surf(d[,c("trans_dist_m", "depth_M", "temp_C")], no.X = 300, no.Y = 300, n = 1, m = 1)
+  dimnames(mba$xyz.est$z) <- list(mba$xyz.est$x, mba$xyz.est$y)
+  df <- reshape2::melt(mba$xyz.est$z, varnames = c("Distance", "Depth"), value.name = "Temp")
+  
+  p <- ggplot(df, aes(Distance, Depth)) + 
+    geom_raster(aes(fill = Temp), interpolate = F) + 
+    geom_contour(aes(z = Temp)) + 
+    geom_point(data = d, aes(trans_dist_m, depth_M), color = "grey") + 
+    # ylim(0,100) + 
+    # xlim(0, 35) +
+    labs(x = "Transect (m)", y = "Depth (m)") + 
+    scale_y_reverse() + 
+    scale_fill_gradientn(colors = viridis_pal()(20), na.value = "transparent") + 
+    theme_light()
+  print(p)
+  ggsave(paste(results, gsub("/",".",ctdDates[i]), ".pdf", sep = ""))
+  temp_list[[i]] = p
+}
+
+#save cruise plots as individual objects
+cruise4.ctd <- temp_list[[1]]
+cruise5.ctd <- temp_list[[2]]
+cruise6.ctd <- temp_list[[3]]
+cruise9.ctd <- temp_list[[4]]
