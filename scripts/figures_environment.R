04bcda5e546a7f24bcbf3386cec26745857dec51
Helen Killeen <hjkilleen@ucdavis.edu>
2022-11-08 10:10:35 -0800

    environment subplots and arrange

--- scripts/figures_environment.R
+++ scripts/figures_environment.R
@@ -7,6 +7,7 @@
 library(tidyverse)
 
 load("data/cruiseDates.rda")
+load("data/environment/hfr/boonHFR.rda")
 load("data/environment/BOONwind/ws.rda")
 load("data/environment/BOONwind/wd.rda")
 #====
@@ -28,6 +29,65 @@ for(i in 1:7){
     ggtitle("Eastward BOON Currents", subtitle = print(cruiseDates[i]))
   ggsave(paste(results,paste(cruiseDates[i]),"_u",".jpg", sep = ""))
 }
+
+boonHFR.df$id <- as.factor(boonHFR.df$id)
+levels(boonHFR.df$id) <- c("b1v", "b1u", "b2v", "b2u", "b6v", "b6u", "b7v", "b7u")
+
+#Manuscript Subplots
+cruise3.hfr <- ggplot(filter(boonHFR.df, datetime_pdt > as.POSIXct(as.numeric(cruiseDates[2]-43200), origin = "1970-01-01"), datetime_pdt < as.POSIXct(as.numeric(cruiseDates[2])+86400, origin = "1970-01-01"), id %in% c("b7u", "b7v"))) + 
+  geom_rect(aes(xmin = as.POSIXct("2019-08-15 11:00:00", tz = "America/Los_Angeles"), xmax = as.POSIXct("2019-08-15 14:00:00", tz = "America/Los_Angeles"), ymin = -Inf, ymax = Inf), alpha = 0.25) +
+  geom_hline(yintercept = 0, color = "grey") +
+  ylim(-25, 20) +
+  geom_line(aes(x = datetime_pdt, y = speed, linetype = id)) + 
+  labs(x = "Local Time", y = "Speed (cm/s)") +
+  ggtitle(print(cruiseDates[2])) +
+  theme_classic() +
+  theme(legend.position = "none")#cruise 3 
+cruise4.hfr <- ggplot(filter(boonHFR.df, datetime_pdt > as.POSIXct(as.numeric(cruiseDates[3]-43200), origin = "1970-01-01"), datetime_pdt < as.POSIXct(as.numeric(cruiseDates[3])+86400, origin = "1970-01-01"), id %in% c("b7u", "b7v"))) + 
+  geom_rect(aes(xmin = as.POSIXct("2019-09-20 08:00:00"), xmax = as.POSIXct("2019-09-20 14:00:00"), ymin = -Inf, ymax = Inf), alpha = 0.25, inherit.aes = FALSE) +
+  geom_hline(yintercept = 0, color = "grey") +
+  ylim(-25, 20) +
+  geom_line(aes(x = datetime_pdt, y = speed, linetype = id)) + 
+  labs(x = "Local Time", y = "Speed (cm/s)") +
+  ggtitle(print(cruiseDates[3])) +
+  theme_classic() +
+  theme(legend.position = "none")#cruise 4
+cruise5.hfr <- ggplot(filter(boonHFR.df, datetime_pdt > as.POSIXct(as.numeric(cruiseDates[4]-43200), origin = "1970-01-01"), datetime_pdt < as.POSIXct(as.numeric(cruiseDates[4])+86400, origin = "1970-01-01"), id %in% c("b7u", "b7v"))) + 
+  geom_rect(aes(xmin = as.POSIXct("2020-06-23 10:45:00"), xmax = as.POSIXct("2020-06-23 15:30:00"), ymin = -Inf, ymax = Inf), alpha = 0.25) +
+  geom_hline(yintercept = 0, color = "grey") +
+  ylim(-25, 20) +
+  geom_line(aes(x = datetime_pdt, y = speed, linetype = id)) + 
+  labs(x = "Local Time", y = "Speed (cm/s)") +
+  ggtitle(print(cruiseDates[4])) +
+  theme_classic() +
+  theme(legend.position = "none")#cruise 5
+cruise6.hfr <- ggplot(filter(boonHFR.df, datetime_pdt > as.POSIXct(as.numeric(cruiseDates[5]-43200), origin = "1970-01-01"), datetime_pdt < as.POSIXct(as.numeric(cruiseDates[5])+86400, origin = "1970-01-01"), id %in% c("b7u", "b7v"))) + 
+  geom_rect(aes(xmin = as.POSIXct("2020-06-30 09:45:00"), xmax = as.POSIXct("2020-06-30 13:30:00"), ymin = -Inf, ymax = Inf), alpha = 0.25, inherit.aes = FALSE) +
+  geom_hline(yintercept = 0, color = "grey") +
+  ylim(-25, 20) +
+  geom_line(aes(x = datetime_pdt, y = speed, linetype = id)) + 
+  labs(x = "Local Time", y = "Speed (cm/s)") +
+  ggtitle(print(cruiseDates[5])) +
+  theme_classic() +
+  theme(legend.position = "none")#cruise 6
+cruise9.hfr <- ggplot(filter(boonHFR.df, datetime_pdt > as.POSIXct(as.numeric(cruiseDates[6]-43200), origin = "1970-01-01"), datetime_pdt < as.POSIXct(as.numeric(cruiseDates[6])+86400, origin = "1970-01-01"), id %in% c("b7u", "b7v"))) + 
+  geom_rect(aes(xmin = as.POSIXct("2020-09-30 09:00:00"), xmax = as.POSIXct("2020-09-30 14:00:00"), ymin = -Inf, ymax = Inf), alpha = 0.25, inherit.aes = FALSE) +
+  geom_hline(yintercept = 0, color = "grey") +
+  ylim(-25, 20) +
+  geom_line(aes(x = datetime_pdt, y = speed, linetype = id)) + 
+  labs(x = "Local Time", y = "Speed (cm/s)") +
+  ggtitle(print(cruiseDates[6])) +
+  theme_classic() +
+  theme(legend.position = "none")#cruise 9
+cruise10.hfr <- ggplot(filter(boonHFR.df, datetime_pdt > as.POSIXct(as.numeric(cruiseDates[7]-43200), origin = "1970-01-01"), datetime_pdt < as.POSIXct(as.numeric(cruiseDates[7])+86400, origin = "1970-01-01"), id %in% c("b7u", "b7v"))) + 
+  geom_rect(aes(xmin = as.POSIXct("2020-10-06 09:00:00"), xmax = as.POSIXct("2020-10-06 13:30:00"), ymin = -Inf, ymax = Inf), alpha = 0.25, inherit.aes = FALSE) +
+  geom_hline(yintercept = 0, color = "grey") +
+  ylim(-25, 20) +
+  geom_line(aes(x = datetime_pdt, y = speed, linetype = id)) + 
+  labs(x = "Local Time", y = "Speed (cm/s)") +
+  ggtitle(print(cruiseDates[7])) +
+  theme_classic() +
+  theme(legend.position = "none")#cruise 10
 #====
 
 #WIND
@@ -43,3 +103,5 @@ for(i in 3:7){
   print(p)
 }
 #====
+
+ggarrange(cruise3.hfr, cruise4.hfr, cruise5.hfr, cruise6.hfr, cruise9.hfr, cruise10.hfr)
