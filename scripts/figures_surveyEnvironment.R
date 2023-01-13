b3edbc8e8490f88a469a25e8e5c5d0bbdb3911e1
Your Name <your@email.com>
2023-01-12 16:24:48 -0800

    merge survey env figure in streamlined scripts

--- scripts/figures_surveyEnvironment.R
+++ scripts/figures_surveyEnvironment.R
@@ -6,9 +6,9 @@
 #====
 library(ggpubr)
 
-source("scripts/figures_environment.R")
-source("scripts/analyze_ctdUnderway.R")
-source("scripts/analyze_ctdProfiles.R")
+source("scripts/figures_windAndCurrents.R")
+source("scripts/4_analysis_ctdProfiles.R")
+source("scripts/5_analysis_ctdUnderway.R")
 #====
 
 #MERGE FIGURES
@@ -16,5 +16,5 @@ source("scripts/analyze_ctdProfiles.R")
 blank <- ggplot() +
   theme_void()#create blanks for missing datatypes
 
-ggarrange(cruise3.hfr, cruise3.uw, blank, cruise4.hfr, cruise4.uw, cruise4.ctd, cruise5.hfr, cruise5.uw, cruise5.ctd, cruise6.hfr, cruise6.uw, cruise6.ctd, cruise9.hfr, cruise9.uw, cruise9.ctd, cruise10.hfr, cruise10.uw, blank, ncol = 3, nrow = 6)
+ggarrange(cruise3.hfr, cruise3.uw, blank, cruise4.hfr, blank, cruise4.ctd, cruise5.hfr, cruise5.uw, cruise5.ctd, cruise6.hfr, cruise6.uw, cruise6.ctd, cruise9.hfr, cruise9.uw, cruise9.ctd, cruise10.hfr, cruise10.uw, blank, ncol = 3, nrow = 6)
 #====
