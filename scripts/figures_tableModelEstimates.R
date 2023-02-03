35d64e08da42c346ac0b9653ec87b37b75b6fa03
Your Name <your@email.com>
2023-02-03 15:44:18 -0800

    create tables for common taxa anovas

--- scripts/figures_tableModelEstimates.R
+++ scripts/figures_tableModelEstimates.R
@@ -12,14 +12,15 @@ library(tikzDevice)
 
 #SET UP
 #====
-df <- read_xlsx("data/table_modelEstimates.xlsx")#load table
-names(df) <- c("Type", "Test statistic1", "p-value1", "Test statistic2", "p-value2")
-df$`p-value1` <- p.adjust(df$`p-value1`, method = "hochberg")
-df$`p-value2` <- p.adjust(df$`p-value2`, method = "hochberg")
+df <- read_csv("output/mainEfx.csv")#load table
+df$location_p <- round(p.adjust(df$location_p, method = "hochberg"), 4)#adjust p value for multiple comparisons
+df$location.depth_p <- round(p.adjust(df$location.depth_p, method = "hochberg"), 4)#adjust p value for multiple comparisons
 names(df) <- c("Type", "Test statistic", "p-value", "Test statistic", "p-value")
 
-df2 <- read_xlsx("data/table_modelContrasts.xlsx")#load table
-names(df2) <- c("Type", "Estimate", "p-value", "Estimate", "p-value", "Estimate", "p-value", "Summary", "Hypothesis")
+df2 <- read_csv("output/pairwise.csv")#load table
+names(df2) <- c("Type", "Estimate", "p-value", "Estimate", "p-value", "Estimate", "p-value")
+df2 <- cbind(df2, data.frame(Summary = c("on<f=off", "on=f>off", "on=f<off", "on=f<off"),#add in summary columns based on estimates and p values
+                             Hypothesis = c("Barrier onshore", "Barrier offshore", "Barrier onshore", "Barrier onshore")))
 #====
 
 #Model Estimates
@@ -38,6 +39,6 @@ df2 %>% #knit table
   kbl() %>% 
   kable_classic() %>% 
   add_header_above(c(" " = 1, "Onshore-Front" = 2, "Onshore-Offshore" = 2, "Front-Offshore" = 2, " " = 2), bold = TRUE) %>% 
-  pack_rows(index = c("Coastal Meroplankton" = 2, "Pelagic Holoplankton" = 3, "Non-Swimming" = 1)) %>% 
+  pack_rows(index = c("Coastal Meroplankton" = 2, "Pelagic Holoplankton" = 2)) %>% 
   row_spec(0, bold = TRUE)
 #====
